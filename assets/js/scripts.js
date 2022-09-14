caecf51ed04eee2ac7b51f17b9e5e5c7e8b6f30b
GomesMilla <gomesmilla@pop-os.localdomain>
2022-09-14 00:02:04 -0300

    :pushspin: Subindo primeiras parte do projeto de desenvolvimento Web - Aula do professor Diógenes

---
+++ assets/js/scripts.js
@@ -0,0 +1,11 @@
+const header = document.querySelector("[data-header]");
+
+const activeElemOnScroll = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", activeElemOnScroll);
